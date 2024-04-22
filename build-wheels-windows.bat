e51ad9ebba3319a09449a9738988ca3b9c075b52
bahugo <barsottihugo@gmail.com>
2024-04-22 19:14:22 +0200

    (WIP) windows compilation

--- build-wheels-windows.bat
+++ build-wheels-windows.bat
@@ -16,20 +16,23 @@ echo "PYTAG=%PYTAG%"
 echo "TAG=%TAG%"
 echo "PATH=%PATH%"
 
+set DEP_DIR="%SCRIPTPATH%\dependencies_tmp"
 set BUILD_DIR="%SCRIPTPATH%\build_dir"
 set BUILD_DEP_DIR="%BUILD_DIR%\dependencies"
 set MEDCOUPLING_BUILD_DIR="%BUILD_DIR%\medcoupling"
 set PYTHON_ROOT=%pythonLocation%
 
+mkdir %DEP_DIR%
 mkdir %BUILD_DEP_DIR%
 mkdir %MEDCOUPLING_BUILD_DIR%
 
 python --version
 python -m pip install build scipy
 
-call "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" x86_amd64
+REM call "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" x86_amd64
+call "C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Auxiliary\Build\vcvarsall.bat" x86_amd64
 
-pushd %BUILD_DIR%
+pushd %DEP_DIR%
 
 :: metis
 echo "---- METIS INSTALLATION ----"
@@ -41,13 +44,13 @@ mkdir METIS\build\windows
 mkdir METIS\build\xinclude
 copy METIS\include\metis.h METIS\build\xinclude
 copy METIS\include\CMakeLists.txt METIS\build\xinclude
-cmake -LAH -S METIS -B build_metis -DCMAKE_INSTALL_PREFIX=%BUILD_DEP_DIR%/metis -DCMAKE_BUILD_TYPE=Release
+cmake -LAH -S METIS -B build_metis -DCMAKE_INSTALL_PREFIX=%BUILD_DIR%\metis -DCMAKE_BUILD_TYPE=Release
 cmake --build build_metis --config Release
-mkdir %BUILD_DEP_DIR%\metis\include
-copy METIS\include\metis.h %BUILD_DEP_DIR%\metis\include
-mkdir %BUILD_DEP_DIR%\metis\lib
-copy build_metis\libmetis\Release\metis.lib %BUILD_DEP_DIR%\metis\lib
-dir %BUILD_DEP_DIR%/metis
+mkdir %BUILD_DIR%\metis\include
+copy METIS\include\metis.h %BUILD_DIR%\metis\include
+mkdir %BUILD_DIR%\metis\lib
+copy build_metis\libmetis\Release\metis.lib %BUILD_DIR%\metis\lib
+dir %BUILD_DIR%\metis
 echo "---- ENDED METIS INSTALLATION ----"
 
 :: libxml2
@@ -55,13 +58,13 @@ echo "---- libxml2 INSTALLATION ----"
 git clone --depth 1 -b v2.10.4 https://github.com/GNOME/libxml2.git libxml2
 pushd libxml2\win32
 cscript configure.js compiler=msvc iconv=no icu=no zlib=no lzma=no python=no ^
-                     prefix=%BUILD_DEP_DIR%\libxml2 include=%BUILD_DEP_DIR%\libxml2\include lib=%BUILD_DEP_DIR%\libxml2\lib
+                     prefix=%BUILD_DIR%\libxml2 include=%BUILD_DIR%\libxml2\include lib=%BUILD_DIR%\libxml2\lib
 nmake /f Makefile.msvc
 nmake /f Makefile.msvc install
 popd
 dir
 echo "---- ENDED libxml2 INSTALLATION ----"
-dir %BUILD_DEP_DIR%\libxml2
+dir %BUILD_DIR%\libxml2
 
 :: boost
 echo "---- BOOST INSTALLATION ----"
@@ -72,7 +75,7 @@ pushd boost_%BOOST_VERSION:.=_%
 call bootstrap.bat
 .\b2 install ^
     --build-dir=build_boost ^
-    --prefix=%BUILD_DEP_DIR%\boost ^
+    --prefix=%BUILD_DIR%\boost ^
     toolset=msvc-14.3 ^
     architecture=x86 ^
     address-model=64 ^
@@ -97,14 +100,14 @@ call bootstrap.bat
 popd
 dir
 echo "---- ENDED BOOST INSTALLATION ----"
-dir %BUILD_DEP_DIR%\boost
+dir boost
 
 :: hdf5
 echo "---- HDF5 INSTALLATION ----"
 git clone --depth 1 -b hdf5-1_10_3 https://github.com/HDFGroup/hdf5.git hdf5
-cmake -LAH -S hdf5 -B build_hdf5 -DCMAKE_INSTALL_PREFIX=%BUILD_DEP_DIR%/hdf5 -DBUILD_TESTING=OFF -DHDF5_BUILD_TOOLS=OFF -DHDF5_BUILD_EXAMPLES=OFF
+cmake -LAH -S hdf5 -B build_hdf5 -DCMAKE_INSTALL_PREFIX=%BUILD_DIR%\hdf5 -DBUILD_TESTING=OFF -DHDF5_BUILD_TOOLS=OFF -DHDF5_BUILD_EXAMPLES=OFF
 cmake --build build_hdf5 --config Release --target install
-dir %BUILD_DEP_DIR%\hdf5
+dir %BUILD_DIR%\hdf5
 echo "---- ENDED HDF5 INSTALLATION ----"
 
 :: med
@@ -119,7 +122,7 @@ echo "extracting med"
 7z x med-%MED_VERSION%.tar > nul
 echo "end extracting med"
 dir
-cmake -LAH -S med-%MED_VERSION% -B build_med -DCMAKE_INSTALL_PREFIX=%BUILD_DEP_DIR%/med -DHDF5_ROOT_DIR=%BUILD_DEP_DIR%/hdf5 ^
+cmake -LAH -S med-%MED_VERSION% -B build_med -DCMAKE_INSTALL_PREFIX=%BUILD_DIR%\med -DHDF5_ROOT_DIR=%BUILD_DIR%\hdf5 ^
   -DMEDFILE_BUILD_TESTS=OFF -DMEDFILE_INSTALL_DOC=OFF
 cmake --build build_med --config Release --target install
 
@@ -127,13 +130,13 @@ git clone --depth 1 -b V%VERSION:.=_% https://git.salome-platform.org/gitpub/too
 
 
 git clone --depth 1 -b V%VERSION:.=_% http://git.salome-platform.org/gitpub/tools/medcoupling.git medcoupling
-cmake -LAH -S medcoupling -B build_medcoupling -DCMAKE_INSTALL_PREFIX=%BUILD_DEP_DIR%/medcoupling ^
-  -DMEDFILE_ROOT_DIR=%BUILD_DEP_DIR%\med ^
-  -DMETIS_ROOT_DIR=%BUILD_DEP_DIR%\metis ^
-  -DHDF5_ROOT_DIR=%BUILD_DEP_DIR%\hdf5 ^
-  -DLIBXML2_ROOT_DIR=%BUILD_DEP_DIR%\libxml2 ^
-  -DBOOST_ROOT_DIR=%BUILD_DEP_DIR%\boost ^
-  -DMEDCOUPLING_BUILD_DOC=OFF -DMEDCOUPLING_BUILD_TESTS=OFF -DCONFIGURATION_ROOT_DIR=%SCRIPTPATH%\configuration ^
+cmake -LAH -S medcoupling -B build_medcoupling -DCMAKE_INSTALL_PREFIX=%BUILD_DIR%\medcoupling ^
+  -DMEDFILE_ROOT_DIR=%BUILD_DIR%\med ^
+  -DMETIS_ROOT_DIR=%BUILD_DIR%\metis ^
+  -DHDF5_ROOT_DIR=%BUILD_DIR%\hdf5 ^
+  -DLIBXML2_ROOT_DIR=%BUILD_DIR%\libxml2 ^
+  -DBOOST_ROOT_DIR=%BUILD_DIR%\boost ^
+  -DMEDCOUPLING_BUILD_DOC=OFF -DMEDCOUPLING_BUILD_TESTS=OFF -DCONFIGURATION_ROOT_DIR=%DEP_DIR%\configuration ^
   -DPYTHON_LIBRARY=%PYTHON_ROOT%\libs\python%ABI:~2%.lib -DPYTHON_INCLUDE_DIR=%PYTHON_ROOT%\include ^
   -DPYTHON_EXECUTABLE=%PYTHON_ROOT%\python.exe ^
   -DMEDCOUPLING_PARTITIONER_METIS=OFF -DMEDCOUPLING_PARTITIONER_SCOTCH=OFF -DMEDCOUPLING_PARTITIONER_METIS=ON -DMEDCOUPLING_USE_64BIT_IDS=OFF
@@ -142,17 +145,17 @@ popd
 dir
 echo "---- ENDED MED INSTALLATION ----"
 
-dir %BUILD_DEP_DIR%\medcoupling
+dir %BUILD_DIR%\medcoupling
 
 popd
 dir
 
 :: build wheel
 echo "---- BUILDING WHEEL ----"
-xcopy /y %BUILD_DEP_DIR%\libxml2\bin\*.dll %MEDCOUPLING_BUILD_DIR%
-xcopy /y %BUILD_DEP_DIR%\hdf5\bin\hdf5.dll %MEDCOUPLING_BUILD_DIR%
-xcopy /y %BUILD_DEP_DIR%\med\lib\medC.dll %MEDCOUPLING_BUILD_DIR%
-xcopy /y %BUILD_DEP_DIR%\medcoupling\lib\*.dll %MEDCOUPLING_BUILD_DIR%
+xcopy /y %BUILD_DIR%\libxml2\bin\*.dll %MEDCOUPLING_BUILD_DIR%
+xcopy /y %BUILD_DIR%\hdf5\bin\hdf5.dll %MEDCOUPLING_BUILD_DIR%
+xcopy /y %BUILD_DIR%\med\lib\medC.dll %MEDCOUPLING_BUILD_DIR%
+xcopy /y %BUILD_DIR%\medcoupling\lib\*.dll %MEDCOUPLING_BUILD_DIR%
 rem  xcopy /y %BUILD_DEP_DIR%\boost\lib\*.dll %MEDCOUPLING_BUILD_DIR%
 
 echo "Checking compilation file integrity"
