24366e6a785d84fe4deb9739e5e0eda97a625461
Julien Schueller <schueller@phimeca.com>
2025-09-02 15:03:59 +0200

    Adapt to cmake 4.x

--- build-wheels-linux.sh
+++ build-wheels-linux.sh
@@ -24,12 +24,15 @@ git clone --depth 1 -b V`echo ${VERSION}|sed "s|\.|_|g"` https://github.com/Salo
 # medcoupling
 pip install scipy
 git clone --depth 1 -b V`echo ${VERSION}|sed "s|\.|_|g"` https://github.com/SalomePlatform/medcoupling.git
-cmake -S medcoupling -B build_medcoupling -LAH -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$PWD/install \
+cmake -LAH -DCMAKE_BUILD_TYPE=Release \
+  -DCMAKE_INSTALL_PREFIX=$PWD/install \
+  -DCMAKE_POLICY_VERSION_MINIMUM=3.5 \
   -DMEDCOUPLING_BUILD_DOC=OFF -DMEDCOUPLING_BUILD_TESTS=OFF -DCONFIGURATION_ROOT_DIR=$PWD/configuration \
   -DPYTHON_EXECUTABLE=/opt/python/${PYTAG}-${ABI}/bin/python \
   -DPYTHON_INCLUDE_DIR=/opt/python/${PYTAG}-${ABI}/include/python${PYVERD} -DPYTHON_LIBRARY=dummy \
   -DMEDCOUPLING_PARTITIONER_SCOTCH=OFF -DMEDCOUPLING_USE_64BIT_IDS=OFF \
-  -DCMAKE_INSTALL_RPATH="${PWD}/install/lib;/usr/local/lib" -DCMAKE_BUILD_WITH_INSTALL_RPATH=ON
+  -DCMAKE_INSTALL_RPATH="${PWD}/install/lib;/usr/local/lib" -DCMAKE_BUILD_WITH_INSTALL_RPATH=ON \
+  -B build_medcoupling -S medcoupling
 cmake --build build_medcoupling --target install
 
 cd install/lib/python*/site-packages
--- build_locally.sh
+++ build_locally.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
+set -xe
 docker build docker/manylinux -t medcoupling/manylinux
 docker run --rm -e MAKEFLAGS='-j8' -v `pwd`:/io medcoupling/manylinux /io/build-wheels-linux.sh 9.14.0 cp310
